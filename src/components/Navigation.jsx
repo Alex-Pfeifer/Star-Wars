32fccde2aea22f783235548052fc999bd8f64ded
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-03-25 10:34:00 +0100

    add NaviItem, Navigation disstructuring

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,13 @@
 //rsc
+import NaviItem from "./NaviItem.jsx";
+import {navItems} from "../utils/constants.js";
+
+
 const Navigation = () => {
     return (
         <nav className="fixed-top mt-2 ms-5">
             <ul className="nav">
-                <li className="nav-item btn btn-danger mx-1">Home</li>
-                <li className="nav-item btn btn-danger mx-1">About me</li>
-                <li className="nav-item btn btn-danger mx-1">Star Wars</li>
-                <li className="nav-item btn btn-danger mx-1">Contact</li>
+                {navItems.map(item => <NaviItem itemTitle={item} key={item}/>)}
             </ul>
         </nav>
     );
