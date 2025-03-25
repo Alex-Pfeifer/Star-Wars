651f36b7b55b28d6f6ae5d7a9b6502018613d4ad
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-03-25 20:42:18 +0100

    add About Me

--- src/components/About Me.jsx
+++ src/components/About Me.jsx
@@ -1,10 +1,41 @@
+import {useEffect, useState} from "react";
+import {aboutMe_url} from "../utils/constants.js";
+import luke_skywalker from "../assets/luke_skywalker.jpg";
 
 
 const AboutMe = () => {
+
+    const [openingAboutMe, setopeningAboutMe] = useState({});
+
+    useEffect(() => {
+        fetch(`${aboutMe_url}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Error while fetching films!');
+                }
+                return res.json()
+            })
+            .then(data => {
+                setopeningAboutMe(data)
+            })
+            .catch(() => setopeningAboutMe('Error while fetching films'));
+    }, []);
+
     return (
-        <div>
-            About Me
-        </div>
+        <p className="farGalaxy d-flex justify-content-between align-items-center">
+            <div className="info">
+                <img src={luke_skywalker} alt="Hero"/>
+            </div>
+            <div className="col-sm-10">
+                <p>Name:{openingAboutMe.name}</p>
+                <p>Gender: {openingAboutMe.gender}</p>
+                <p>Hair Color:{openingAboutMe.hair_color}</p>
+                <p>Height:{openingAboutMe.height} cm</p>
+                <p>Eye Color:{openingAboutMe.eye_color}</p>
+                <p>Mass:{openingAboutMe.mass} kg</p>
+                <p>Birth Year:{openingAboutMe.birth_year}</p>
+            </div>
+        </p>
     );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,6 +10,11 @@ import friend9 from "../assets/friend9.jpg";
 
 export const base_url = 'https://sw-info-api.herokuapp.com'
 
+export const aboutMe_url = 'https://sw-info-api.herokuapp.com/v1/peoples/1'
+export const aboutMePicture_url = 'https://sw-info-api.herokuapp.com/'
+
+
+
 export const friends = [friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9];
 export const navItems = ['Home', 'About Me', 'Star Wars', 'Contact'];
 export const starWarsInfo =  "Star Wars ist ein Film-Franchise, dessen Geschichte mit dem 1977 erschienenen Kinofilm Krieg der Sterne (Originaltitel: Star Wars) begann. Schöpfer von Star Wars ist der Drehbuchautor, Produzent und Regisseur George Lucas. Im Jahr 2012 verkaufte Lucas seine Firma Lucasfilm, mitsamt den Rechten an Star Wars, an die Walt Disney Company. Im Kern besteht Star Wars aus drei Filmtrilogien: der Originaltrilogie, die neben Krieg der Sterne (heutiger Titel Eine neue Hoffnung) die Filme Das Imperium schlägt zurück (1980) und Die Rückkehr der Jedi-Ritter (1983) umfasst; der Prequel-Trilogie, die in der Zeit vor der Originaltrilogie spielt, mit den Filmen Die dunkle Bedrohung (1999), Angriff der Klonkrieger (2002) und Die Rache der Sith (2005); und der Sequel-Trilogie mit den Filmen Das Erwachen der Macht (2015), Die letzten Jedi (2017) und Der Aufstieg Skywalkers (2019)."
